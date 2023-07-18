2551474b2bad3478f5e6c06a17f1630cf03677c8
Himarshipatel <62707959+Himarshipatel@users.noreply.github.com>
2023-07-18 19:48:51 +0530

    commit

---
+++ src/components/repository.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import {
+    Card,
+    CardTitle,
+    CardText,
+    Button,
+    ButtonGroup,
+    Col,
+    Row,
+    Container,
+} from "reactstrap";
+import axios from "axios";
+
+const Repository = () => {
+    const [issue, setissue] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://api.github.com/search/repositories?q=Q")
+            .then((res) => {
+                setissue(res.data.items);
+            });
+    }, []);
+    console.log(issue);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const recordsPerPage = 5;
+    const lastIndex = currentPage * recordsPerPage;
+    const firstIndex = lastIndex - recordsPerPage;
+    const records = issue.slice(firstIndex, lastIndex);
+    const page = Math.ceil(issue.length / recordsPerPage);
+    console.log(page);
+    const numbers = [...Array(page + 1)].slice(1);
+    console.log(numbers);
+
+    function prePage() {
+        if (currentPage !== firstIndex) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+    function changeCPage() {
+        setCurrentPage();
+    }
+    function nextPage() {
+        if (currentPage !== lastIndex) {
+            setCurrentPage(currentPage + 1);
+        } else {
+            setCurrentPage(currentPage);
+        }
+    }
+
+    return (
+        <>
+            {records.map((item, index) => (
+                <ul key={index}>
+                    <Container>
+                        <Card
+                            outline
+                            style={{ padding: "25px 25px 25px 25px" }}
+                        >
+                            <Row>
+                                <Col sm="3">
+                                    <img
+                                        src={item.owner.avatar_url}
+                                        alt="image"
+                                        style={{
+                                            width: "12rem",
+                                            height: "12rem",
+                                        }}
+                                    />
+                                </Col>
+                                <Col sm="9">
+                                    <CardTitle tag="h2">
+                                        {item.full_name}
+                                    </CardTitle>
+                                    <CardText tag="h3">
+                                        {item.description}
+                                    </CardText>
+                                    <Row>
+                                        <ButtonGroup
+                                            style={{
+                                                marginTop: "1rem",
+                                                marginBottom: "2rem",
+                                            }}
+                                        >
+                                            <Button color="primary" outline>
+                                                {" "}
+                                                {item.stargazers_count} Star
+                                            </Button>
+                                            <Button color="primary" outline>
+                                                {" "}
+                                                {item.open_issues} Issue
+                                            </Button>{" "}
+                                        </ButtonGroup>
+                                    </Row>
+
+                                    <CardText tag="h6">
+                                        {" "}
+                                        Last pushed by {
+                                            item.pushed_at
+                                        } Owner {item.owner.login}
+                                    </CardText>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Container>
+                </ul>
+            ))}
+
+            <div className="data">
+                <ul className="pagination">
+                    <li className="page-item">
+                        <a href="#" className="page-link" onClick={prePage}>
+                            Prev
+                        </a>
+                    </li>
+                    {numbers.map((n, i) => (
+                        <li
+                            className={`page-item ${
+                                currentPage === n ? "active" : ""
+                            }`}
+                            key={i}
+                        >
+                            <a
+                                href="#"
+                                className="page-item"
+                                onClick={changeCPage}
+                            >
+                                {n}
+                            </a>
+                        </li>
+                    ))}
+                    <li className="page-item">
+                        <a href="#" className="page-link" onClick={nextPage}>
+                            Next
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+};
+export default Repository;
